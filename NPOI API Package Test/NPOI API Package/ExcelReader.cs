0830682a3965b79f0877a23232da8013c707b519
GYP <1368024175@qq.com>
2025-11-03 10:16:38 +0800

    [add] ExcelReader.cs add "Read" method

--- NPOI API Package Test/NPOI API Package/ExcelReader.cs
+++ NPOI API Package Test/NPOI API Package/ExcelReader.cs
@@ -48,5 +48,79 @@ namespace NPOI_API_Package
         /// <param name="sheetName">工作表名称</param>
         public ExcelReader(IWorkbook workbook, string sheetName) : this(workbook, workbook.GetSheet(sheetName))
         { }
+
+        /// <summary>
+        /// 读取数据
+        /// </summary>
+        /// <param name="firstRowIndex">首行索引(从0开始)</param>
+        /// <param name="hasHeader">是否包含头部信息</param>
+        /// <param name="columnConfigs">列信息配置集合</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">当firstRowIndex小于0时抛出</exception>
+        /// <exception cref="ArgumentNullException">当hasHeader为false且columnConfigs为null或空时抛出</exception>
+        public DataTable Read(int firstRowIndex = 0, bool hasHeader = true, IEnumerable<ColumnConfigAttribute>? columnConfigs = null)
+        {
+            //校验首行索引值
+            if (firstRowIndex < 0)
+            {
+                throw new ArgumentException($"无效的首行索引值, firstRowIndex: {firstRowIndex}", nameof(firstRowIndex));
+            }
+
+            //如果工作表包含头部, 且列配置集合为空, 则自动创建列集合
+            if (hasHeader && (columnConfigs == null || !columnConfigs.Any()))
+            {
+                var tempColumnConfigs = new List<ColumnConfigAttribute>();
+
+                //访问工作表头部信息
+                var headerRow = sheet.GetRow(firstRowIndex);
+
+                //遍历单元格
+                foreach (ICell cell in headerRow.Cells)
+                {
+                    var colConfig = new ColumnConfigAttribute
+                    { 
+                        ColumnMapping = cell.ToString(),
+                        ColumnIndex = cell.ColumnIndex,
+                    };
+                    tempColumnConfigs.Add(colConfig);
+                }
+
+                columnConfigs = tempColumnConfigs;
+            }
+            else
+            {
+                //如果工作表不包含头部, 且列配置集合依旧为空, 则抛出异常
+                if (!hasHeader && (columnConfigs == null || !columnConfigs.Any()))
+                {
+                    throw new ArgumentNullException(nameof(columnConfigs), "当工作表不包含头部信息时, 列配置集合不可以为空！");
+                }
+            }
+
+            var table = new DataTable();
+
+            //对table添加列
+            foreach (var columnConfig in columnConfigs!)
+            {
+                var column = new DataColumn(columnConfig.ColumnMapping);
+                table.Columns.Add(column);
+            }
+
+            //对table添加数据
+            int startRowIndex = firstRowIndex + (hasHeader ? 1 : 0);
+            foreach (IRow row in sheet)
+            {
+                if (row.RowNum >= startRowIndex)
+                {
+                    DataRow dataRow = table.NewRow();
+                    foreach (var columnConfig in columnConfigs)
+                    {
+                        dataRow[columnConfig.ColumnMapping] = row.GetCell(columnConfig.ColumnIndex);
+                    }
+                    table.Rows.Add(dataRow);
+                }
+            }
+
+            return table;
+        }
     }
 }
