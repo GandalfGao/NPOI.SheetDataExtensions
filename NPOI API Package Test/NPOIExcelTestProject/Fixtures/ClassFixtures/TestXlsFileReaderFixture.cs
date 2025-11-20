93182bcec843c81f0069233417420f52e9ae08b6
xiaopengyunyou <1368024175@qq.com>
2025-11-20 22:36:22 +0800

    [add] 添加测试文件夹具类 [add] 添加单元格扩展测试类

---
+++ NPOI API Package Test/NPOIExcelTestProject/Fixtures/ClassFixtures/TestXlsFileReaderFixture.cs
@@ -0,0 +1,51 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPOIExcelTestProject.Fixtures.ClassFixtures
+{
+    /// <summary>
+    /// 测试Xls文件读取器夹具
+    /// </summary>
+    public class TestXlsFileReaderFixture : IDisposable
+    {
+        /// <summary>
+        /// Excel文件对象
+        /// </summary>
+        private readonly IWorkbook workbook;
+        /// <summary>
+        /// 包含布尔值数据的行对象
+        /// </summary>
+        private readonly IRow boolRow;
+        /// <summary>
+        /// 包含数字值数据的行对象
+        /// </summary>
+        private readonly IRow numRow;
+
+        public TestXlsFileReaderFixture()
+        {
+            workbook = WorkbookFactory.Create("TestExcelFiles\\TestXlsFile.xls");
+
+            int i = 0;
+            var sheet = workbook.GetSheetAt(0);
+            boolRow = sheet.GetRow(i++);
+            numRow = sheet.GetRow(i++);
+        }
+
+        /// <summary>
+        /// 包含布尔值数据的行对象属性
+        /// </summary>
+        public IRow BoolRow => boolRow;
+
+        /// <summary>
+        /// 包含数字值数据的行对象属性
+        /// </summary>
+        public IRow NumRow => numRow;
+
+        public void Dispose()
+        {
+            workbook.Dispose();
+        }
+    }
+}
---
+++ NPOI API Package Test/NPOIExcelTestProject/Tests/CellExtensionTest.cs
@@ -0,0 +1,63 @@
+﻿using NPOI.SS.UserModel;
+using NPOI_API_Package;
+using NPOIExcelTestProject.Fixtures.ClassFixtures;
+using NPOI_API_Package;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace NPOIExcelTestProject.Tests
+{
+    public class CellExtensionTest : IClassFixture<TestXlsFileReaderFixture>
+    {
+        private readonly TestXlsFileReaderFixture testXlsFileReaderFixture;
+        private readonly ITestOutputHelper outputHelper;
+
+        public CellExtensionTest(TestXlsFileReaderFixture testXlsFileReaderFixture, ITestOutputHelper outputHelper)
+        {
+            this.testXlsFileReaderFixture = testXlsFileReaderFixture;
+            this.outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Test_GetCellValue_WhenCellIsNull()
+        {
+            ICell? cell = null;
+            var val = cell!.GetCellValue();
+            Assert.Equal(string.Empty, val);
+        }
+
+        [Fact]
+        public void Test_GetCellValue_WhenCellIsBoolean()
+        {
+            var row = testXlsFileReaderFixture.BoolRow;
+            var trueCell = row.GetCell(1);
+            var falseCell = row.GetCell(2);
+
+            var trueVal = trueCell.GetCellValue();
+            Assert.IsType<bool>(trueVal);
+            Assert.Equal(true, trueVal);
+
+            var falseVal = falseCell.GetCellValue();
+            Assert.IsType<bool>(falseVal);
+            Assert.Equal(false, falseVal);
+        }
+
+        [Fact]
+        public void Test_GetCellValue_WhenCellIsNumeric()
+        {
+            var row = testXlsFileReaderFixture.NumRow;
+            var intCell = row.GetCell(1);
+            var doubleCell = row.GetCell(2);
+
+            var intVal = intCell.GetCellValue();
+            Assert.IsType<double>(intVal);
+            Assert.Equal(123.0, intVal);
+
+            var doubleVal = doubleCell.GetCellValue();
+            Assert.IsType<double>(doubleVal);
+            Assert.Equal(123.456, doubleVal);
+        }
+    }
+}
