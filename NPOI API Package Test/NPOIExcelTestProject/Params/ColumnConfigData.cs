b35655b922dff87b4c6f8d4794554ecbb83d9baa
GYP <1368024175@qq.com>
2025-12-11 13:37:33 +0800

    [mod] 修改代码

---
+++ NPOI API Package Test/NPOIExcelTestProject/Params/ColumnConfigData.cs
@@ -0,0 +1,14 @@
+﻿using NPOI_API_Package;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPOIExcelTestProject.Params
+{
+    /// <summary>
+    /// 列配置参数类
+    /// </summary>
+    public class ColumnConfigData
+    {
+    }
+}
--- NPOI API Package Test/NPOIExcelTestProject/Tests/ExcelReaderXlsxTest.cs
+++ NPOI API Package Test/NPOIExcelTestProject/Tests/ExcelReaderXlsxTest.cs
@@ -3,6 +3,7 @@ using NPOIExcelTestProject.Fixtures.CollectionFixtures;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit.Abstractions;
 
 namespace NPOIExcelTestProject.Tests
 {
@@ -13,10 +14,12 @@ namespace NPOIExcelTestProject.Tests
     public class ExcelReaderXlsxTest
     {
         private readonly TestFileReaderXlsxFixture testXlsxFileReaderFixture;
+        private readonly ITestOutputHelper outputHelper;
 
-        public ExcelReaderXlsxTest(TestFileReaderXlsxFixture testXlsxFileReaderFixture)
+        public ExcelReaderXlsxTest(TestFileReaderXlsxFixture testXlsxFileReaderFixture, ITestOutputHelper outputHelper)
         {
             this.testXlsxFileReaderFixture = testXlsxFileReaderFixture;
+            this.outputHelper = outputHelper;
         }
 
         /// <summary>
@@ -26,7 +29,25 @@ namespace NPOIExcelTestProject.Tests
         public void Test_Read_WhenFirstRowIndexIsNegative()
         {
             var excelReader = new ExcelReader(testXlsxFileReaderFixture.Workbook, testXlsxFileReaderFixture.Sheet3);
-            Assert.Throws<ArgumentException>(() => excelReader.Read(firstRowIndex: -1));
+            var ex = Assert.Throws<ArgumentException>(() => excelReader.Read(firstRowIndex: -1));
+            outputHelper.WriteLine(ex.Message);
         }
+
+        /// <summary>
+        /// 测试当 hasHeader 为 false 且 columnConfigs 为 null 或空集合时，Read 方法应抛出 ArgumentNullException 异常。
+        /// </summary>
+        [Fact]
+        public void Test_Read_WhenHasHeaderIsFalseAndColumnConfigsIsNullOrEmpty()
+        {
+            var excelReader = new ExcelReader(testXlsxFileReaderFixture.Workbook, testXlsxFileReaderFixture.Sheet3);
+            var ex = Assert.Throws<ArgumentNullException>(() => excelReader.Read(hasHeader: false, columnConfigs: null));
+            outputHelper.WriteLine(ex.Message);
+
+            var ex2 = Assert.Throws<ArgumentNullException>(() => excelReader.Read(hasHeader: false, columnConfigs: []));
+            outputHelper.WriteLine(ex2.Message);
+        }
+
+        public void Test_Read_WhenHasHeaderIsTrue(IEnumerable<ColumnConfigAttribute>? columnConfigs)
+        { }
     }
 }
